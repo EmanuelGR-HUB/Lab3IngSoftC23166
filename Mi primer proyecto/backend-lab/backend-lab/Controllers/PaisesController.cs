81245510ff115a7237a2650266b3173f3c8c4189
EmanuelGR <emanuel.garcia@ucr.ac.cr>
2025-04-12 18:18:34 -0600

    Se añade el laboratorio 4

---
+++ Mi primer proyecto/backend-lab/backend-lab/Controllers/PaisesController.cs
@@ -0,0 +1,24 @@
+﻿using backend_lab.Handlers;
+using backend_lab.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_lab.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaisesController : ControllerBase
+    {
+        private readonly PaisHandler _paisesHandler;
+        public PaisesController()
+        { 
+            _paisesHandler = new PaisHandler();
+        }
+        [HttpGet]
+        public List<PaisModel> Get()
+        {
+            var paises = _paisesHandler.ObtenerPaises();
+            return paises;
+        }
+    }
+}
